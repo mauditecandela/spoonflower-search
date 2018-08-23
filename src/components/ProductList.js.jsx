ee20a39d8a9e42acfb0b61e7f58689ccf2324209
mauditecandela <candela.jimenez@gmail.com>
2018-08-23 12:20:45 +0200

    Accesses the products state through the props inside of ProductList Iterates through this result set and displays the names of the products to the user

--- src/components/ProductList.js.jsx
+++ src/components/ProductList.js.jsx
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 
 class ProductList extends Component {
   render() {
+    const products = this.props.products.map((product) => {
+      return (
+        <div key={product.id}>
+          <div className="product-name">{product.name}</div>
+        </div>
+      )
+    })
+    return (
+      <div>
+        {products}
+      </div>
+    )
   }
+}
 
 export default ProductList;
