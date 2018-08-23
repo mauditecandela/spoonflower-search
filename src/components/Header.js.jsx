a84c416079b819c60878f271e112d23bc34d5263
mauditecandela <candela.jimenez@gmail.com>
2018-08-23 16:03:00 +0200

    Adds alt text to the spoonflower logo

--- src/components/Header.js.jsx
+++ src/components/Header.js.jsx
@@ -6,7 +6,7 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        <img src="https://d3u3xhd9tobx8a.cloudfront.net/static/global/spoonflower_logo.svg" className="spoonflower-logo"></img>
+        <img src="https://d3u3xhd9tobx8a.cloudfront.net/static/global/spoonflower_logo.svg" className="spoonflower-logo" alt="Spoonflower logo"></img>
         <h1>Custom printed fabric and more</h1>
       </div>
     )
