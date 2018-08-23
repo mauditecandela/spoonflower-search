8baa33d9049997b8fccbe9d2ea570a48065ab15f
mauditecandela <candela.jimenez@gmail.com>
2018-08-23 12:22:10 +0200

    Adds h1 and h2 as titles of the page

--- src/components/Header.js.jsx
+++ src/components/Header.js.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
+    return (
+      <div>
+        <h1>Spoonflower</h1>
+        <h2>Custom printed fabric and more</h2>
+      </div>
+    )
   }
-
+}
 export default Header;
